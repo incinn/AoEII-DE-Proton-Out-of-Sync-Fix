41c1e7114f64649614704b3e34136867c3264d44
Barnz <hi@barnz.dev>
2021-02-16 12:15:33 +0000

    absolute user path

--- fix.sh
+++ fix.sh
@@ -3,7 +3,7 @@ command -v wget >/dev/null 2>&1 || { echo "wget not found, aborting." >&2; exit 
 command -v cabextract >/dev/null 2>&1 || { echo "cabextract not found, aborting." >&2; exit 1; }
 
 URL="https://aka.ms/vs/16/release/vc_redist.x64.exe"
-DEST="~/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32"
+DEST="/home/${USER}/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32"
 
 cd $(mktemp -d)
 wget ${URL} 2>/dev/null || curl -O ${URL}
