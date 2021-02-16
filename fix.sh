02836d29c958e69787e0b49cbade7c1911267f02
Barnz <hi@barnz.dev>
2021-02-16 10:36:02 +0000

    tweaks

--- fix.sh
+++ fix.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd /tmp/aoe-fix/
+cd $(mktemp -d)
 wget "https://aka.ms/vs/16/release/vc_redist.x64.exe"
 cabextract vc_redist.x64.exe
 cabextract a10
 
-sudo cp ucrtbase.dll ~/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32
+cp ucrtbase.dll ~/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32
 
-rm -rf /tmp/aoe-fix/
