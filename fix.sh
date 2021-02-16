1f30d2c56fe619b33ef05abad5824bbbe2802ab3
Barnz <hi@barnz.dev>
2021-02-16 12:15:18 +0000

    dependency check

--- fix.sh
+++ fix.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+command -v wget >/dev/null 2>&1 || { echo "wget not found, aborting." >&2; exit 1; }
+command -v cabextract >/dev/null 2>&1 || { echo "cabextract not found, aborting." >&2; exit 1; }
+
 URL="https://aka.ms/vs/16/release/vc_redist.x64.exe"
 DEST="~/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32"
 
