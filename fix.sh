e54b54b6334cf1b36ade48b366ab11b3190a4a8b
Barnz <hi@barnz.dev>
2021-02-16 10:53:26 +0000

    support for wget or curl

--- fix.sh
+++ fix.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+URL="https://aka.ms/vs/16/release/vc_redist.x64.exe"
+DEST="~/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32"
 
 cd $(mktemp -d)
-wget "https://aka.ms/vs/16/release/vc_redist.x64.exe"
+wget ${URL} 2>/dev/null || curl -O ${URL}
+
 cabextract vc_redist.x64.exe
 cabextract a10
 
-cp ucrtbase.dll ~/.steam/steam/steamapps/compatdata/813780/pfx/drive_c/windows/system32
-
+cp ucrtbase.dll ${DEST}
